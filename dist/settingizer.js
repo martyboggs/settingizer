64dfaa364c46bf5a4bcd8962a38e054196b38540
martyboggs <whack47@gmail.com>
2018-12-09 11:05:44 -0800

    remove log

--- dist/settingizer.js
+++ dist/settingizer.js
@@ -399,7 +399,6 @@ function create_settings(data, model) {
 			for (prop in option) {
 				if (prop.match(/^sc_/)) {
 					// for some props, just look at the last one
-					if (prop === 'sc_label') console.log(prop, i, index.length);
 					if (['sc_label'].indexOf(prop) === -1 || i === index.length) {
 						modelActive[prop] = option[prop];
 					}
